82840200f2cc5e17b3358e538124e4252130ed3a
Ignc <60786122+halftime@users.noreply.github.com>
2026-04-23 00:28:02 +0200

    migrations

---
+++ Migrations/20260422222246_Initial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace price_web_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Investments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Symbol = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    MorningStarId = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    ISIN = table.Column<string>(type: "TEXT", maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Investments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PriceRecords",
+                columns: table => new
+                {
+                    symbol = table.Column<string>(type: "TEXT", nullable: false),
+                    date = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    InvestmentId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PriceRecords", x => new { x.symbol, x.date });
+                    table.ForeignKey(
+                        name: "FK_PriceRecords_Investments_InvestmentId",
+                        column: x => x.InvestmentId,
+                        principalTable: "Investments",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Investments_Symbol",
+                table: "Investments",
+                column: "Symbol",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PriceRecords_InvestmentId",
+                table: "PriceRecords",
+                column: "InvestmentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PriceRecords");
+
+            migrationBuilder.DropTable(
+                name: "Investments");
+        }
+    }
+}
